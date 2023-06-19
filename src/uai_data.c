ac5d37603e84278fb3fc8c4dbf2a5392d05ecfa7
bvpav <78971592+bvpav@users.noreply.github.com>
2023-06-19 09:38:05 +0300

    fixed vsplit bug

--- src/uai_data.c
+++ src/uai_data.c
@@ -283,15 +283,29 @@ UAI_Status df_create_vsplit(DataFrame *src, DataFrame *dst, size_t take, enum Da
     assert(take <= src->rows);
     assert(sampling == DATAFRAME_SAMPLE_SEQ && "Only sequential sampling is supported");
 
+    size_t new_rows = src->rows + !!src->header;
+    DataCell *new_cellbuf = malloc(sizeof *new_cellbuf * new_rows * take);
+    if (!new_cellbuf)
+        return UAI_ERRNO;
+
     UAI_Status err = df_copy(src, dst);
     if (err)
-        return err;
+        goto error_post_new_cellbuf;
 
     src->cols -= take;
-    for (DataCell *r = dst->cellbuf; r < dst->cellbuf + dst->rows * dst->cols; r += dst->cols)
-        memcpy(r, r + dst->cols - take, sizeof *r * take);
+    for (DataCell *s = dst->cellbuf + dst->cols - take, *d = new_cellbuf; d < new_cellbuf + new_rows * take; d += take, s += dst->cols)
+        memcpy(d, s, sizeof *d * take);
     dst->cols = take;
+    free(dst->cellbuf);
+    dst->cellbuf = new_cellbuf;
+    DataCell **rows = dst->header ? dst->data-1 : dst->data;
+    for (size_t r=0; r<new_rows; ++r)
+        rows[r] = new_cellbuf + r * take;
     return UAI_OK;
+
+error_post_new_cellbuf:
+    free(new_cellbuf);
+    return err;
 }
 
 UAI_Status df_create(DataFrame *df, size_t num_rows, size_t num_cols)
